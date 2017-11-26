b064e14487453115ca9e5475f9b1fdb123fe878b
jmmobley <jason.mobley1978@gmail.com>
2017-11-26 12:47:43 -0600

    Initial port of parser to core. Issues remain with tests and build.

---
+++ Core/UAParserCore.Tests/ParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+using YamlDotNet.Core;
+
+namespace UAParserCore.Tests
+{
+  public class ParserTests
+  {
+    [Fact]
+    public void can_get_default_parser()
+    {
+      Parser parser = Parser.GetDefault();
+      Assert.NotNull(parser);
+    }
+
+    [Fact]
+    public void can_get_parser_from_input()
+    {
+      string yamlContent = this.GetTestResources("UAParserCore.Tests.Regexes.regexes.yaml");
+      Parser parser = Parser.FromYaml(yamlContent);
+      Assert.NotNull(parser);
+    }
+  }
+}
